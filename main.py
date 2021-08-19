7c4838c979887374ed0be749899f4687a8b9c512
Leonardo D'Angelo Goncalves <leonardodg2084@gmail.com>
2021-08-19 19:57:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from typing import List
 
 DB = "slack"
 MSG_COLLECTION = "messages"
-
+MONGODB_SERVER = "mongodb://mongodb.iaac0506.com.br:27017/"
 
 # Classe que ira definir o schema do mongodb
 class Message(BaseModel):
@@ -30,7 +30,7 @@ def get_status():
 @app.get("/channels", response_model=List[str])
 def get_channels():
     """Get all channels in list form."""
-    with MongoClient() as client:
+    with MongoClient(MONGODB_SERVER) as client:
         msg_collection = client[DB][MSG_COLLECTION]
         distinct_channel_list = msg_collection.distinct("channel")
         return distinct_channel_list
@@ -39,7 +39,7 @@ def get_channels():
 @app.get("/messages/{channel}", response_model=List[Message])
 def get_messages(channel: str):
     """Get all messages for the specified channel."""
-    with MongoClient() as client:
+    with MongoClient(MONGODB_SERVER) as client:
         msg_collection = client[DB][MSG_COLLECTION]
         msg_list = msg_collection.find({"channel": channel})
         response_msg_list = []
@@ -51,7 +51,7 @@ def get_messages(channel: str):
 @app.post("/post_message", status_code=status.HTTP_201_CREATED)
 def post_message(message: Message):
     """Post a new message to the specified channel."""
-    with MongoClient() as client:
+    with MongoClient(MONGODB_SERVER) as client:
         msg_collection = client[DB][MSG_COLLECTION]
         result = msg_collection.insert_one(message.dict())
         ack = result.acknowledged
